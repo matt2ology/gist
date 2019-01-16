b49280be5913803c25c7678bb15d34820f106d5e
matt2ology <34324060+matt2ology@users.noreply.github.com>
2019-01-15 22:51:58 -0800

    Learning the basics of organization and layout windows and widgets

---
+++ python/TheNewBostonYouTubeTutorial/Python-GUI-with-Tkinter/pythonTkinter2_OrganizingLayout.py
@@ -0,0 +1,10 @@
+# Part of the core python language
+from tkinter import *
+
+""" Add a basic window with close sign, minimize, and maximize (Main Window)"""
+root = Tk()
+
+
+
+""" Enable the Main Window to stay on the screen """
+root.mainloop()
