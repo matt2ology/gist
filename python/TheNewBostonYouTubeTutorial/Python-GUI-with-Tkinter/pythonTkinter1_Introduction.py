b45c1a5a9181bd297d0a21cd1c86fd5cdca57711
matt2ology <34324060+matt2ology@users.noreply.github.com>
2019-01-15 22:45:33 -0800

    Learning GUI with Python from thenewboston

---
+++ python/TheNewBostonYouTubeTutorial/Python-GUI-with-Tkinter/pythonTkinter1_Introduction.py
@@ -0,0 +1,8 @@
+# Part of the core python language
+from tkinter import *
+
+""" Add a basic window with close sign, minimize, and maximize """
+root = Tk()
+theLable = Label(root, text="This is the text in the label")
+theLable.pack()
+root.mainloop()
