a49f1b3601f4147fbaccafdbde6e4d89048e9f95
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2019-01-20 14:06:27 -0800

    intit commit

--- python/TheNewBostonYouTubeTutorial/Python-GUI-with-Tkinter/pythonTkinter4_GridLayout.py
+++ python/TheNewBostonYouTubeTutorial/Python-GUI-with-Tkinter/pythonTkinter4_GridLayout.py
@@ -4,17 +4,17 @@ from tkinter import *
 """ Add a basic window with close sign, minimize, and maximize (Main Window)"""
 root = Tk()
 
-userNamePrompt = label(root, Text="Enter Username")
-passwordPrompt = label(root, Text="Enter Password")
+userNamePrompt = Label(root, text="Enter Username")
+passwordPrompt = Label(root, text="Enter Password")
 
-userNameField = entry(root)
-passwordField = entry(root)
+userNameField = Entry(root)
+passwordField = Entry(root)
 
 userNamePrompt.grid(row=0)
 passwordPrompt.grid(row=1)
 
-userNameField.grid(row=0,column=1)
-passwordPrompt.grid(row=1,column=1)
+userNameField.grid(row=0, column=1)
+passwordField.grid(row=1, column=1)
 
 """ Enable the Main Window to stay on the screen """
 root.mainloop()
